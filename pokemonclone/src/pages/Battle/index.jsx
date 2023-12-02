dc28a7f54774b2a7275137f8ade72ea6c3b15378
CaueAlvim <caue.eyti@gmail.com>
2023-12-02 17:45:39 -0300

    initial organization of how battle scene will shown

--- pokemonclone/src/pages/Battle/index.jsx
+++ pokemonclone/src/pages/Battle/index.jsx
@@ -1,12 +1,35 @@
 import { useState } from 'react'
+import LinearProgress from '@mui/material/LinearProgress';
+
 
 
 function Battle() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-    </>
+    <div className="flex w-screen min-h-screen">
+      <div className="flex flex-col w-[50rem] h-[30rem] bg-amber-300">
+        <div className=' h-[10rem] w-full flex'>
+          <div className='bg-lime-800 flex flex-col h-full w-[50%] p-[3rem]'>
+            oi
+            <LinearProgress sx={{width: "40%"}} variant="determinate" value={10} />
+          </div>
+          <div className='bg-red-300 h-full w-[50%]'>
+
+          </div>
+        </div>
+        <div className=' h-[10rem] w-full flex'>
+          <div className='bg-lime-400 h-full w-[50%]'>
+
+          </div>
+          <div className='bg-lime-800 flex flex-col h-full w-[50%] p-[3rem]'>
+            oi
+            <LinearProgress sx={{width: "40%"}} variant="determinate" value={10} />
+          </div>
+        </div>
+        <div className='bg-blue-300 h-[10rem] w-full'></div>
+      </div>
+
+    </div>
   )
 }
 
